c66a450452b74fd7e0de20d05c6a3fdd3ed519bf
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2021-05-23 20:47:17 +0800

    Welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +129,9 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
+
+<h1>Dimensi Kini</h1>
+<h2>SSO Client 1</h2>
+
+<a href="login"> Login with SSO </a>
