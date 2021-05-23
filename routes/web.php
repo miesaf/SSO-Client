ea19fe9ce7a31b360a33d117d6adcd4229c6bf6d
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2021-05-23 20:47:29 +0800

    Route setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function (Request $request) {
+    $request->session()->put("state", $state = Str::random(40));
+
+    $query = http_build_query([
+        "client_id" => "937fc8e4-587d-4228-838c-cb36d6c4b671",
+        "redirect_uri" => "http://localhost/SSO-Client-1/public/callback",
+        "response_type" => "code",
+        "state" => $state
+    ]);
+
+    return redirect("http://localhost/SSO/public/oauth/authorize?" . $query);
+});
+
+Route::get('/callback', function (Request $request) {
+    $state = $request->session()->pull("state");
+
+    throw_unless(strlen($state) > 0 && $state == $request->state, InvalidArgumentException::class);
+
+    $response = Http::asForm()->post(
+        "http://localhost/SSO/public/oauth/token",
+        [
+            "grant_type" => "authorization_code",
+            "client_id" => "937fc8e4-587d-4228-838c-cb36d6c4b671",
+            "client_secret" => "1Xk4c6rZCIzru7B2S11HjBwwwlS7HJjeLK1JNMnv",
+            "redirect_uri" => "http://localhost/SSO-Client-1/public/callback",
+            "code" => $request->code
+        ]
+    );
+
+    return $response->json();
 });
